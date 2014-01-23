c838013369997c003357b559c801b6dec63d592c
bshain <shainbarbara@gmail.com>
2014-01-23 13:51:39 -0500

    all tests pass

---
+++ 09_array_extensions/array_extensions.rb
@@ -0,0 +1,18 @@
+class Array
+
+	def sum
+		result = 0
+		self.each{|x| result += x }
+		result
+	end
+
+	def square
+		self.collect!{|x| x**2}
+	end
+
+	def square!
+		self.collect!{|x| x**2}
+	end
+
+
+end
