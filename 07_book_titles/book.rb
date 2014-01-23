ad631b20a672a627f184f3bb342401b50393c7e1
bshain <shainbarbara@gmail.com>
2014-01-23 01:32:45 -0500

    refactored a bit

--- 07_book_titles/book.rb
+++ 07_book_titles/book.rb
@@ -2,17 +2,15 @@ class Book
 
 attr_reader :title 
 
-	def initialize
-		@lowercase = ["the","a","an","and","in","of"]
-	end
 	
 	def title=(string)
 		@title = string
 	end
 
 	def title
+		lowercase = ["the","a","an","and","in","of"]
 		array = @title.split(" ")
-		array.each {|x| x.capitalize! unless (@lowercase.include? x)}
+		array.each {|x| x.capitalize! unless (lowercase.include? x)}
 		array[0].capitalize!
 		array.join(" ")
 	end
